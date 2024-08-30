11e8e6a1e9fd7a8fe0fbf9bea6a2eafc8a418670
tiedan <1226207253@qq.com>
2024-08-30 22:46:10 +0800

    看了代码，增加了一点注释

--- code/3/0351.py
+++ code/3/0351.py
@@ -35,11 +35,16 @@ d2l.use_svg_display()
 # 通过ToTensor实例将图像数据从PIL类型变换成32位浮点数格式，
 # 并除以255使得所有像素的数值均在0～1之间
 trans = transforms.ToTensor()
+
+#加载训练集
 mnist_train = torchvision.datasets.FashionMNIST(root="../data", train=True, transform=trans, download=True)
+
+#加载测试集
 mnist_test = torchvision.datasets.FashionMNIST(root="../data", train=False, transform=trans, download=True)
 
 print(len(mnist_train), len(mnist_test))
 print(mnist_train[0][0].shape)
 
 X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
-show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y));
+
+show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y))
