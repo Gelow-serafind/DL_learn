a1e90652dc4f435a47006c5efd3f6002aa9dd301
tiedanHP <1226207253@qq.com>
2024-09-04 18:38:49 +0800

    增加交叉熵损失函数，但是还没看懂

--- code/3/0364.py
+++ code/3/0364.py
@@ -3,3 +3,9 @@ import torch
 y = torch.tensor([0, 3])
 y_hat = torch.tensor([[0.1, 0.3, 0.6, 0.8], [0.3, 0.2, 0.5, 0.9]])
 print(y_hat[[0, 1], y])
+
+def cross_entropy(y_hat, y):
+    return - torch.log(y_hat[range(len(y_hat)), y])
+
+cross_entropy(y_hat, y)
+
